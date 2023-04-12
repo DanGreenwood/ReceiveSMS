4b1a2658457a3395ce7ba718b8103d0927f72457
DanGreenwood <dr.dan.greenwood@gmail.com>
2023-04-11 21:01:17 -0600

    added pandas

--- secondTry.py
+++ secondTry.py
@@ -1,12 +1,22 @@
 from flask import Flask, request
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.DataFrame()
 
 @app.route('/json', methods=['POST'])
 def handle_request():
     if request.headers['Content-Type'] == 'application/json':
         json_data = request.json
         print(json_data)
+        # Use json_normalize() to convert the JSON data to a dataframe
+        new_data = pd.json_normalize(json_data)
+
+        # Append the new data to the existing dataframe
+        global df
+        df = df.append(new_data, ignore_index=True)
+        print(df)
         return 'JSON posted'
     else:
         return 'Unsupported Media Type', 415
